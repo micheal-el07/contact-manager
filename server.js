89ef59718ca5870ce72a2d12c64be746e5749852
Mikhail <mikhailkamal00@gmail.com>
2024-10-21 14:20:34 +0800

    update server entry file

--- server.js
+++ server.js
@@ -1,16 +1,35 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const { PORT, MONGO_URL } = require("./config/env.config")
+const bodyParser = require("body-parser");
+const asyncHandler = require("express-async-handler");
+
+const { router } = require("./routers");
+const { PORT, MONGO_URL } = require("./config/env.config");
+const bodyParser = require("body-parser");
 
 const app = express();
-// const MONGOURL = MONGO_URL;
 
-mongoose.connect(MONGO_URL).then(() => {
-    console.log("Database is connected successfully.");
-}).catch((error) => console.log(error));
+app.use(express.json());
+app.use.bodyParser(bodyParser.urlencoded({ extended: false }));
 
+// Create connection to mongodb atlas
+mongoose
+  .connect(MONGO_URL)
+  .then(() => {
+    console.log("Database connected.");
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+
+// GET placeholder
 app.get("/", (req, res) => {
   res.send("Hey");
+});
+
+// Throwing 404 error for invalid endpoint
+app.all("*", (req, res) => {
+  res.status(404).json({ status: "failed", message: "Invalid endpoint" });
 });
 
 app.listen(PORT, () => {
