76191df103bb1d02a5ca82e0fcb944055d0051d5
Mikhail <mikhailkamal00@gmail.com>
2024-11-06 15:50:38 +0800

    add routing to user and contact

--- routers/index.js
+++ routers/index.js
@@ -1,5 +1,10 @@
 const { Router } = require("express");
+const contactRouter = require("../routers/contact.router");
+const userRouter = require("../routers/users.router");
 
 const router = Router();
+
+router.use("/contacts", contactRouter);
+router.use("/users", userRouter);
 
 module.exports = { router };
